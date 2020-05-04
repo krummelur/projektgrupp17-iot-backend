dd75bf76bb280e2b51e1af30aae29f7197180811
Magnus <krummelur@gmail.com>
2020-05-04 21:49:50 +0200

    oops. wouldn't compile in poduction because fromData only implemented for debug

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -5,6 +5,11 @@ pub mod videos;
 use serde::Deserialize;
 use rocket_contrib::json::{JsonValue};
 use serde_json::json;
+use rocket::data::{self, FromDataSimple};
+use rocket::{Request, Data, Outcome::*};
+use rocket::http::Status;
+use std::io;
+use std::io::Read;
 
 #[derive(Deserialize)]
 pub struct RegisterBody {
@@ -13,10 +18,30 @@ pub struct RegisterBody {
 }
 
 #[derive(Deserialize)]
+pub struct StrCont {
+    data: String,
+}
+
+
+
+#[derive(Deserialize)]
 pub struct LogMessage {
     error: bool,
     message: String,
 }
+
+impl FromDataSimple for StrCont {
+    type Error = io::Error;
+    #[inline(always)]
+    fn from_data(_: &Request, data: Data) -> data::Outcome<Self,  io::Error> {
+        let mut value = String::new();
+        match data.open().read_to_string(&mut value) {
+            Ok(_) => Success(StrCont{data: value}),
+            Err(e) => Failure((Status::BadRequest, e))
+        }
+    }
+}
+
 
 /**
  * Gets the current API version / checks if api is alive
@@ -47,10 +72,10 @@ fn log_message_json(body: LogMessage) -> Result<JsonValue, JsonValue>  {
 }
 
 #[post("/logs", data = "<body>")]
-pub fn log_message(body: String) -> Result<JsonValue, JsonValue>  {
-    match serde_json::from_str::<LogMessage>(&body[..]) {
+pub fn log_message(body: StrCont) -> Result<JsonValue, JsonValue>  {
+    match serde_json::from_str::<LogMessage>(&body.data[..]) {
         Ok(val) => log_message_json(val),
-        Err(_) => log_message_str(body)
+        Err(_) => log_message_str(body.data)
     }
 }
 
